59df36228f56e989bdfa5b54aaf7c6c56cd7935b
Juditsatu <100708807+Juditsatu@users.noreply.github.com>
2022-06-07 13:03:17 +0200

    added functions

--- src/films.js
+++ src/films.js
@@ -1,28 +1,38 @@
 // Exercise 1: Get the array of all directors.
 function getAllDirectors(array) {
-  let result =  ???;
-  console.log("EXERCICE 1 ->", result);
+  let result =  array.map(x => x.director);
   return result;
 }
 
 // Exercise 2: Get the films of a certain director
 function getMoviesFromDirector(array, director) {
- 
+  let result = array.filter(array => array.director == director);
+  return result;
 }
 
 // Exercise 3: Calculate the average of the films of a given director.
 function moviesAverageOfDirector(array, director) {
-  
+  director = getMoviesFromDirector(array, director);
+  let averageScore = director.reduce(
+    (score, average) => score + average.score, 0) / director.length;
+  return averageScore;
 }
 
 // Exercise 4:  Alphabetic order by title 
 function orderAlphabetically(array) {
-  
+  let title = array.map(x => x.title);
+  let titleOrderAlphabetical = title.sort((a, b) => a.localeCompare(b));
+  let result = titleOrderAlphabetical.slice(0, 20)
+  return result;
 }
 
 // Exercise 5: Order by year, ascending
-function orderByYear() {
-
+function orderByYear(array) {
+  let sortYear = array.sort((a, b) => b.year - a.year, 'en', { numeric: true });
+  let result = array.forEach((e) => {
+    console.log(`${e.title}: ${e.year}`);
+  });
+  return result;
 }
 
 // Exercise 6: Calculate the average of the movies in a category
