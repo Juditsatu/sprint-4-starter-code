c403dd31ca312468d16c458dfc2a93b664d74da0
Judisatu <judit.sant@gmail.com>
2022-06-11 13:04:58 +0200

    All test are OK

--- src/films.js
+++ src/films.js
@@ -17,46 +17,45 @@ function moviesAverageOfDirector(array, director) {
   director = getMoviesFromDirector(array, director);
   let averageScore = director.reduce(
     (score, average) => score + average.score, 0) / director.length;
-  return averageScore;
+  return Number(averageScore.toFixed(2));
 }
 
 // Exercise 4:  Alphabetic order by title 
 function orderAlphabetically(array) {
   let title = array.map(x => x.title);
   let titleOrderAlphabetical = title.sort((a, b) => a.localeCompare(b));
-  let result = titleOrderAlphabetical.slice(0, 20)
+  let result = titleOrderAlphabetical.slice(0, 20);
   return result;
 }
 
 // Exercise 5: Order by year, ascending
-function orderByYear(array, year) {
-  array= array.sort((a, b) => a.title.localeCompare(b.title));
-  year = array.sort((a, b) => a.year - b.year);
-  let result = year.slice(0, 20);
-  //l'ordre de pel·lícules és correcte, però el test ascending order falla...
-  //console.log(result)
+function orderByYear(array) {
+  let result = [...array].sort((a, b) => {
+    if (a.year > b.year) return 1;
+    if (b.year > a.year) return -1;
+
+    if (a.title > b.title) return 1;
+    if (b.title > a.title) return -1;
+  });
   return result;
 }
 
 // Exercise 6: Calculate the average of the movies in a category
 function moviesAverageByCategory(array, genre) {
-  genre = array.filter(array => array.genre == genre);
+  genre = array.filter(array => array.genre == genre && array.score != '');
   let averageScore = genre.reduce(
-    (score, average) => score + average.score, 0);
-  let averageMovieScore = averageScore / genre.length;
-    //no poso el .toFixed(2) pero al test surt passed??
-    //el metode reduce ignora els score "buits" pero el test falla
-  return averageMovieScore; 
+    (score, average) => score + average.score, 0) / genre.length;
+  return averageScore; 
 }
 
 // Exercise 7: Modify the duration of movies to minutes
 function hoursToMinutes(array) {
-  const movieMinutes = array.map((e) => {
-    let arrDuration = e.duration.replace(/[a-z ]/g,"");
+  const movieMinutes = array.map((movie) => {
+    let arrDuration = movie.duration.replace(/[a-z ]/g,"");
     let hours = Number(arrDuration[0]) > 0 ? Number(arrDuration[0]) * 60 : 0;
     let minutes = arrDuration.length > 2 ? Number(arrDuration[1] + arrDuration[2]) : 0;
     let hourToMinute = Number(hours + minutes);
-    return {...e, duration: hourToMinute};
+    return {...movie, duration: hourToMinute};
   });
 
   return movieMinutes;
@@ -65,10 +64,9 @@ function hoursToMinutes(array) {
 // Exercise 8: Get the best film of a year
 function bestFilmOfYear(array, year) {
   let yearFilm = array.filter(array => array.year == year).sort();
-  let result = yearFilm.slice(0, 1)
-  return result;
+  let bestFilm = yearFilm.slice(0, 1)
+  return bestFilm;
 }
-
 
 
 // The following is required to make unit tests work.
